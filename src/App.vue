822b4508e0c97f175a4ffaccdaa73d3e09916766
Jason Peraza <jason.dev.127@gmail.com>
2022-09-16 11:46:03 -0600

    :art: Update navigation

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,22 @@
 <template>
-  <div id="app" class="bg-gradient-to-r from-green-400 to-blue-500">
-    <div id="nav" class="flex flex-row justify-center items-center">
-      <router-link to="/" v-slot="{ href, navigate, isActive, isExactActive }">
-        <div
-          aria-haspopup="true"
-          :class="[
-            'py-2',
-            { 'font-bold border-b-4': isActive && isExactActive },
-          ]"
-        >
-          <a :href="href" @click="navigate">
-            <!-- <i class="menu-icon flaticon-user"></i> -->
-            <span class="text-md">Crear To-Do</span>
-          </a>
-        </div>
-      </router-link>
-      <router-link
-        to="/list"
-        v-slot="{ href, navigate, isActive, isExactActive }"
-      >
-        <div
-          aria-haspopup="true"
-          :class="[
-            'py-2',
-            { 'font-bold border-b-4': isActive && isExactActive },
-          ]"
-        >
-          <a :href="href" @click="navigate">
-            <!-- <i class="menu-icon flaticon-user"></i> -->
-            <span class="text-md">To-Do List</span>
-            <!-- <span
-              class="mx-2 px-1 bg-opacity-100 text-blue-500 text-sm font-semibold bg-white rounded-sm"
-              >5</span
-            > -->
-          </a>
-        </div>
-      </router-link>
-    </div>
+  <div id="app" class="bg-gradient-to-r from-green-400 to-blue-500 font-sans">
+    <Navbar />
     <div class="flex items-center justify-center">
       <router-view />
     </div>
   </div>
 </template>
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>ipt
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   min-height: 100vh;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  @apply text-white mx-3 p-2;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+    <nav id="nav" class="flex flex-row justify-center items-center p-10">
+        <div
+            class="bg-white p-1 rounded-xl flex flex-row justify-evenly items-center"
+        >
+            <router-link
+                to="/create"
+                v-slot="{ href, navigate, isActive, isExactActive }" custom
+            >
+                <div
+                    aria-haspopup="true"
+                    :class="[
+                        'py-2 px-3 rounded-lg',
+                        { 'font-bold bg-gray-200': isActive && isExactActive },
+                    ]"
+                >
+                    <a :href="href" @click="navigate">
+                        <!-- <i class="menu-icon flaticon-user"></i> -->
+                        <span class="text-md text-gray-600">Add To-Do</span>
+                    </a>
+                </div>
+            </router-link>
+            <router-link
+                to="/list"
+                v-slot="{ href, navigate, isActive, isExactActive }" custom
+            >
+                <div
+                    aria-haspopup="true"
+                    :class="[
+                        'py-2 px-3 rounded-lg',
+                        { 'font-bold bg-gray-200': isActive && isExactActive },
+                    ]"
+                >
+                    <a :href="href" @click="navigate">
+                        <!-- <i class="menu-icon flaticon-user"></i> -->
+                        <span class="text-md text-gray-600">To-Do List</span>
+                        <span
+                            class="ml-2 p-1 px-2 bg-opacity-100 text-white text-sm font-bold bg-blue-500 rounded-xl"
+                            >{{ getTodosLength }}</span
+                        >
+                    </a>
+                </div>
+            </router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+    name: "NavBar",
+    computed: {
+        ...mapGetters(["getTodosLength"]),
+    },
+};
+</script>
